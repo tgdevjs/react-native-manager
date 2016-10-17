326354614c4923be89be8c0dd82ce9c366d62bbb
tg <tgdevjs@gmail.com>
2016-10-17 12:30:18 -0400

    Added mapStateToProps inside EmployeeList component.

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ListView } from 'react-native';
 import { connect } from 'react-redux';
 import { employeesFetch } from '../actions';
+import _ from 'lodash';
 
 class EmployeeComponent extends Component {
   componentWillMount() {
+    console.log('EmployeeComponent componetWillMount')
     this.props.employeesFetch();
+    this.createDataSource(this.props);
+  }
+  componentWillReceiveProps(nextProps) {
+    console.log('EmployeeComponent componentWillReceiveProps')
+    this.createDataSource(nextProps);
+  }
+  createDataSource({ employees }) {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+    this.dataSource = ds.cloneWithRows(employees);
   }
   render() {
+    console.log('EmployeeComponent: ',this.props.employees);
     return (
       <View>
         <Text>Employee List</Text>
@@ -22,4 +36,11 @@ class EmployeeComponent extends Component {
   }
 }
 
-export default connect(null, { employeesFetch } )(EmployeeComponent);
+const mapStateToProps = state => {
+  const employees = _.map(state.employees, (val, uid) => {
+    return { ...val, uid };
+  });
+  return { employees };
+}
+
+export default connect(mapStateToProps, { employeesFetch } )(EmployeeComponent);
