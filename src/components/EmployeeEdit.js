dbef047ca7bd2dee8e6d8ecc885ba37dacbd431a
tg <tgdevjs@gmail.com>
2016-10-17 15:59:18 -0400

    Added button handler and intial connected intial data for EmployeeEdit component.

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
-import { connect } from 'redux';
+import { connect } from 'react-redux';
+import _ from 'lodash';
 import { Card, CardSection, Button } from './common';
 import EmployeeForm from './EmployeeForm';
+import { employeeUpdate } from '../actions';
 
 class EmployeeList extends Component {
+  componentWillMount() {
+    _.each(this.props.employee, (value, prop) => {
+      this.props.employeeUpdate({ prop, value });
+    })
+  }
+  onButtonPress() {
+    const { name, phone, shift } = this.props;
+    console.log('EmployeeEdit: onButtonPress: ',name,phone,shift);
+  }
   render() {
     return (
       <Card>
         <EmployeeForm />
         <CardSection>
-          <Button>Save Changes</Button>
+          <Button onPress={this.onButtonPress.bind(this)}>Save Changes</Button>
         </CardSection>
       </Card>
     )
   }
 }
+const mapStateToProps = state => {
+  const { name, phone, shift } = state.employeeForm;
 
-export default connect()(EmployeeList);
+  return { name, phone, shift };
+};
+
+export default connect( mapStateToProps, { employeeUpdate } )(EmployeeList);
