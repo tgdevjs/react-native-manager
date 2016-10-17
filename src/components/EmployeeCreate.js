03179c74a1908ff8869bcbe7fa2353199d82294a
tg <tgdevjs@gmail.com>
2016-10-16 20:51:35 -0400

    Added Picker for shift inside EmployeeCreate.

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Picker } from 'react-native';
 import { Card, CardSection, Input, Button } from './common';
 import { connect } from 'react-redux';
 import { employeeUpdate } from '../actions';
@@ -26,6 +27,18 @@ class EmployeeCreate extends Component {
         </CardSection>
 
         <CardSection>
+          <Picker style={{flex: 1}}
+            selecteValue={this.props.shift}
+            onValueChanged={value => this.props.employeeUpdate({ prop: 'shift', value })}
+          >
+            <Picker.Item label="Monday" value="" />
+            <Picker.Item label="Tuesday" value="Tuesday" />
+            <Picker.Item label="Wednesday" value="Wednesday" />
+            <Picker.Item label="Thursday" value="Thursday" />
+            <Picker.Item label="Friday" value="Friday" />
+            <Picker.Item label="Saturday" value="Saturday" />
+            <Picker.Item label="Sunday" value="Sunday" />
+          </Picker>
         </CardSection>
 
         <CardSection>
