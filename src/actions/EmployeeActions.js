da042b690345541e4b486aa110790cf25be67b1c
tg <tgdevjs@gmail.com>
2016-10-17 11:24:56 -0400

    Added employeesFetch action creator.

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,5 +1,9 @@
 import firebase from 'firebase';
-import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE } from './types';
+import {
+  EMPLOYEE_UPDATE,
+  EMPLOYEE_CREATE,
+  EMPLOYEES_FETCH_SUCCESS
+} from './types';
 import { Actions } from 'react-native-router-flux';
 
 export const employeeUpdate = ({ prop, value }) => {
@@ -18,6 +22,20 @@ export const employeeCreate = ({ name, phone, shift }) => {
       .then( () => {
         dispatch({ type: EMPLOYEE_CREATE });
         Actions.employeeList( {type: 'reset'} );
+      });
+  }
+}
+
+export const employeesFetch = () => {
+  const { currentUser } = firebase.auth();
+
+  return (dispatch) => {
+    firebase.database().ref(`/users/${ currentUser.uid }/employees`)
+      .on('value', snapshot => {
+        dispatch({
+          type: EMPLOYEES_FETCH_SUCCESS,
+          payload: snapshot.val()
+        });
       });
   }
 }
