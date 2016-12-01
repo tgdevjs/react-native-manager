e9e9382b1a5087b3d505d869673e5bde2a1a703d
tg <tgdevjs@gmail.com>
2016-12-01 16:28:20 -0500

    Added readme images.

--- readme.md
+++ readme.md
@@ -3,3 +3,10 @@ react native employee manager app using redux and firebase
 
 udemy:
 https://www.udemy.com/the-complete-react-native-and-redux-course
+
+
+<img src="./readmeAssets/RNManager-login.png" alt="RNManager-login" border="5" width="300">
+
+<img src="./readmeAssets/RNManager-list.png" alt="RNManager-list" border="5" width="300">
+
+<img src="./readmeAssets/RNManager-detail.png" alt="RNManager-detail" border="5" width="300">
--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -4,7 +4,7 @@ import Communications from 'react-native-communications';
 import _ from 'lodash';
 import { Card, CardSection, Button, Confirm } from './common';
 import EmployeeForm from './EmployeeForm';
-import { employeeUpdate, employeeSave } from '../actions';
+import { employeeUpdate, employeeSave, employeeDelete } from '../actions';
 
 class EmployeeList extends Component {
   state = { showModal: false };
@@ -22,6 +22,15 @@ class EmployeeList extends Component {
   onTextPress() {
     const { phone, shift } = this.props;
     Communications.text( phone, `Your next shift is on ${ shift }.`);
+  }
+
+  onAccept() {
+    const { uid } = this.props.employee;
+    this.props.employeeDelete( { uid } );
+  }
+
+  onDecline() {
+    this.setState({ showModal: false });
   }
 
   render() {
@@ -50,6 +59,8 @@ class EmployeeList extends Component {
 
         <Confirm
             visible={ this.state.showModal }
+            onAccept={this.onAccept.bind(this)}
+            onDecline={this.onDecline.bind(this)}
         >
           Are you sure you want to delete this?
         </Confirm>
@@ -63,4 +74,4 @@ const mapStateToProps = state => {
   return { name, phone, shift };
 };
 
-export default connect( mapStateToProps, { employeeUpdate, employeeSave } )(EmployeeList);
+export default connect( mapStateToProps, { employeeUpdate, employeeSave, employeeDelete } )(EmployeeList);
